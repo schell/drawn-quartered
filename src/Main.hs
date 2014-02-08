7758a920e232d2bb2948eb2820ddc366f9f8ee9f
Schell Scivally <efsubenovex@gmail.com>
2014-02-08 14:35:13 -0800

    Can draw text in the scene graph!

--- src/Main.hs
+++ src/Main.hs
@@ -3,38 +3,56 @@ module Main where
 
 import           Graphics.UI.UrzaStarter as Urza
 import           Control.Concurrent.MVar
-import           Graphics.Rendering.OpenGL hiding (DisplayList, Matrix, renderer, get)
+import           Graphics.Rendering.OpenGL hiding (SceneList, Matrix, renderer, get)
 import           Graphics.Rendering.Draw
 import           Graphics.Rendering.Draw.Math
-import           Graphics.Rendering.Draw.Utils
 import           Graphics.Rendering.Draw.Shader.Text as T
 import           Graphics.Rendering.Draw.Shader.Shape as S
 import           Graphics.Rendering.Draw.Text.Renderer
 import           Graphics.Rendering.Draw.Text.Types
-import           Graphics.Rendering.Draw.LoadTexture
-import           Control.Concurrent
 import           Control.Lens
 import           Control.Monad
 import           Control.Monad.State
 import           System.Exit
 import           System.Directory
-import           Data.Maybe
 import           Data.Monoid
-import qualified Data.List as L
 import qualified Data.Map as M
-import Debug.Trace
+import qualified Data.IntMap as I
 
 
-data NodeRenderer = NodeRenderer { _rendererWindowSize :: Size
-                                 , _rendererModelview  :: Matrix GLfloat
-                                 , _rendererShapeShader:: ShapeShaderProgram
-                                 , _rendererTextShader :: TextShaderProgram
-                                 , _rendererTextAtlas  :: Atlas
+type SizeAtlas = I.IntMap Atlas
+
+
+type FontAtlas = M.Map FilePath SizeAtlas
+
+
+lookupAtlas :: FilePath -> Int -> FontAtlas -> Maybe Atlas
+lookupAtlas font size fAtlas = do
+    sizeAtlas <- M.lookup font fAtlas
+    I.lookup size sizeAtlas
+
+
+insertAtlas :: FilePath -> Int -> Atlas -> FontAtlas -> FontAtlas
+insertAtlas font size atls fAtls = M.insert font sizeAtls' fAtls
+    where  mSizeAtls = M.lookup font fAtls
+           sizeAtls  = case mSizeAtls of
+                           Nothing -> mempty
+                           Just a  -> a
+           sizeAtls' = I.insert size atls sizeAtls
+
+
+
+data NodeRenderer = NodeRenderer { _rendererWindowSize  :: Size
+                                 , _rendererModelview   :: Matrix GLfloat
+                                 , _rendererShapeShader :: ShapeShaderProgram
+                                 , _rendererTextShader  :: TextShaderProgram
+                                 , _rendererFontAtlas   :: FontAtlas
+                                 , _rendererFontDir     :: FilePath
                                  }
 makeLenses ''NodeRenderer
 
 
-type Draw = NodeRenderer -> IO ()
+type Draw = NodeRenderer -> IO NodeRenderer
 
 
 data Scale = Scale GLfloat GLfloat
@@ -43,31 +61,47 @@ data Scale = Scale GLfloat GLfloat
 data Rotation = Rotation GLfloat
 
 
-data DisplayNode = DisplayNode { _nodeName     :: String
-                               , _nodeTexture  :: Maybe TextureObject
-                               , _nodePosition :: Position
-                               , _nodeSize     :: Size
-                               , _nodeScale    :: Scale
-                               , _nodeRotation :: Rotation
-                               , _nodeDraws    :: [Draw]
-                               , _nodeChildren :: Int
+data SceneText = SceneText { _textFont   :: FilePath
+                               , _textWidth  :: Int
+                               , _textString :: String
+                               , _textColor  :: Color4 GLfloat
                                }
-makeLenses ''DisplayNode
+makeLenses ''SceneText
 
 
-emptyNode :: DisplayNode
-emptyNode = DisplayNode { _nodeName = ""
-                        , _nodeTexture = Nothing
-                        , _nodePosition = Position 0 0
-                        , _nodeSize = Size 0 0
-                        , _nodeScale = Scale 1 1
-                        , _nodeRotation = Rotation 0
-                        , _nodeDraws = []
-                        , _nodeChildren = 0
-                        }
+emptySceneText :: SceneText
+emptySceneText = SceneText { _textFont = ""
+                               , _textWidth = 12
+                               , _textString = ""
+                               , _textColor = Color4 0 0 0 1
+                               }
 
 
-nodeTransform :: DisplayNode -> Matrix GLfloat
+data SceneNode = SceneNode { _nodeName      :: String
+                           , _nodeTexture   :: Maybe TextureObject
+                           , _nodePosition  :: Position
+                           , _nodeSize      :: Size
+                           , _nodeScale     :: Scale
+                           , _nodeRotation  :: Rotation
+                           , _nodeDraws     :: [Draw]
+                           , _nodeChildren  :: Int
+                           }
+makeLenses ''SceneNode
+
+
+emptyNode :: SceneNode
+emptyNode = SceneNode { _nodeName = ""
+                      , _nodeTexture = Nothing
+                      , _nodePosition = Position 0 0
+                      , _nodeSize = Size 0 0
+                      , _nodeScale = Scale 1 1
+                      , _nodeRotation = Rotation 0
+                      , _nodeDraws = []
+                      , _nodeChildren = 0
+                      }
+
+
+nodeTransform :: SceneNode -> Matrix GLfloat
 nodeTransform n =
     let Position tx ty = n^.nodePosition
         Rotation rz    = n^.nodeRotation
@@ -78,16 +112,52 @@ nodeTransform n =
     in foldl multiply (identityN 4) [t,r,s]
 
 
-type DisplayList = [DisplayNode]
+type SceneList = [SceneNode]
 
 
-node :: State DisplayNode DisplayList -> DisplayList
+node :: State SceneNode SceneList -> SceneList
 node s = let (list,n) = runState s emptyNode
              n' = n{_nodeChildren = length list}
          in n':list
 
 
-drawShapes :: IO () -> State DisplayNode ()
+drawText :: State SceneText () -> State SceneNode ()
+drawText s = nodeDraws %= (++ [renderText (execState s emptySceneText)])
+
+
+data Scene = Scene { _sceneNodeRenderer :: NodeRenderer
+                   , _sceneList         :: SceneList
+                   }
+
+
+renderText :: SceneText -> Draw
+renderText t r = do
+    let Size w h = r^.rendererWindowSize
+        font     = r^.rendererFontDir ++ t^.textFont
+        size     = t^.textWidth
+        text     = t^.textString
+        pj = orthoMatrix 0 (fromIntegral w) 0 (fromIntegral h) 0 1
+        mv = r^.rendererModelview
+        ts = r^.rendererTextShader
+        mA = lookupAtlas font size (r^.rendererFontAtlas)
+    -- Get a text renderer.
+    tr <- case mA of
+        Just a  -> return $ TextRenderer ts a
+        Nothing -> do atls <- makeAtlas font $ fromIntegral size
+                      return $ TextRenderer ts atls
+    -- Load that text up into the renderer.
+    tr' <- loadCharMap tr $ t^.textString
+    -- Set up the draw.
+    currentProgram $= Just (ts^.T.program)
+    ts^.T.setProjection $ concat pj
+    ts^.T.setModelview $ concat mv
+    ts^.setTextColor $ t^.textColor
+    putStrLn text
+    _ <- drawTextAt' tr (Position 0 0) (t^.textString)
+    return $ r & rendererFontAtlas %~ (insertAtlas font size (tr'^.atlas))
+
+
+drawShapes :: IO () -> State SceneNode ()
 drawShapes f = nodeDraws %= (++ [renderShapes f])
 
 
@@ -102,21 +172,24 @@ renderShapes f r = do
     s^.S.setProjection $ concat pj
     s^.S.setModelview $ concat mv
     f
+    return r
 
 
-renderList :: NodeRenderer -> DisplayList -> IO DisplayList
-renderList _ [] = return []
+renderList :: NodeRenderer -> SceneList -> IO NodeRenderer
+renderList r [] = return r
 renderList r (n:list) = do
     let nodes = take (n^.nodeChildren) list
         rest  = drop (n^.nodeChildren) list
+        mv    = r^.rendererModelview
         r'    = r & rendererModelview %~ (`multiply` nodeTransform n)
-    -- Draw this node
-    forM_ (n^.nodeDraws) $ \f -> f r'
+    -- Draw this node and update our renderer.
+    -- This allows us to load fonts and text as we go along.
+    r'' <- foldM (\rndr f -> f rndr) r' (n^.nodeDraws)
     -- Draw subnodes with this modelview.
-    nodes' <- renderList r' nodes
+    r''' <- renderList r'' nodes
     -- Draw the rest of the nodes without this modelview.
-    rest' <- renderList r rest
-    return $ [n] ++ nodes' ++ rest'
+    renderList (r''' & rendererModelview .~ mv) rest
+
 
 
 data App = App { _appCursor :: Position }
@@ -126,19 +199,27 @@ newApp :: App
 newApp = App (Position 0 0)
 
 
-gui :: DisplayList
+gui :: SceneList
 gui = node $ do
-    -- Set some initial goodies.
-    nodeSize .= Size 150 150
-    nodePosition .= Position 50 50
-    -- Do some drawing into the node.
-    drawShapes $ strokePath $ do
-        setColor $ Color4 1 0 0 1
-        moveTo 0 0
-        lineTo 150 150
-        rectangleAt 0 0 150 150
-    -- Return the child nodes.
-    return $ (node $ do
+    nodeRotation .= Rotation (pi/4)
+    nodePosition .= Position 100 100
+    drawText $ do
+        textWidth .= 32
+        textColor .= Color4 1 0 0 0.5
+        textFont .= "Deutsch.ttf"
+        textString .= "Drawn & Quartered"
+    return $ node $ do
+        -- Set some initial goodies.
+        nodeSize .= Size 150 150
+        nodePosition .= Position 50 50
+        -- Do some drawing into the node.
+        drawShapes $ strokePath $ do
+            setColor $ Color4 1 0 0 1
+            moveTo 0 0
+            lineTo 150 150
+            rectangleAt 0 0 150 150
+        -- Return the child nodes.
+        return $ (node $ do
                  nodePosition .= Position 50 50
                  drawShapes $ fillPath $ do
                      setColor $ Color4 0 1 0 1
@@ -147,7 +228,7 @@ gui = node $ do
              ++
              (node $ do
                  nodePosition .= Position 50 100
-                 nodeRotation .= (Rotation 1)
+                 nodeRotation .= (Rotation (-1))
                  drawShapes $ fillPath $ do
                      setColor $ Color4 0 0 1 1
                      rectangleAt 0 0 50 50
@@ -164,16 +245,23 @@ gui = node $ do
 
 main :: IO ()
 main = do
-    wvar <- initUrza (100,100) (800,600) "Purely Functional Display List"
-    font <- fmap (++ "/assets/font/Deutsch.ttf") getCurrentDirectory
-    print font
+    wvar <- initUrza (100,100) (800,600) "Purely Functional Scene List"
+    fontDir <- fmap (++ "/assets/font/") getCurrentDirectory
 
-    let load tr = loadCharMap tr $ map toEnum [33..126]
-    (TextRenderer tshader atlas) <- makeTextRenderer font 32 >>= load
-
+    tshader <- makeTextShaderProgram
     sshader <- makeShapeShaderProgram
 
-    guiVar <- newMVar gui
+    let renderer = NodeRenderer { _rendererWindowSize  = Size 800 600
+                                , _rendererModelview   = identityN 4
+                                , _rendererShapeShader = sshader
+                                , _rendererTextShader  = tshader
+                                , _rendererFontAtlas   = mempty
+                                , _rendererFontDir     = fontDir
+                                }
+
+    sceneVar <- newMVar $ Scene { _sceneNodeRenderer = renderer
+                                , _sceneList = gui
+                                }
 
     forever $ do
         pollEvents
@@ -187,15 +275,11 @@ main = do
         clearColor $= Color4 0.13 0.13 0.13 1
         clear [ColorBuffer, DepthBuffer]
 
-        let renderer = NodeRenderer { _rendererWindowSize  = Size winW winH
-                                    , _rendererModelview   = identityN 4
-                                    , _rendererShapeShader = sshader
-                                    , _rendererTextShader  = tshader
-                                    , _rendererTextAtlas   = atlas
-                                    }
-
         -- Render the display list.
-        modifyMVar_ guiVar $ renderList renderer
+        modifyMVar_ sceneVar $ \(Scene r ns) -> do
+            -- Render the scene after updating the renderer's window size.
+            r' <- renderList (r & rendererWindowSize .~ Size winW winH) ns
+            return $ Scene r' ns
 
         swapBuffers window
         shouldClose <- windowShouldClose window
